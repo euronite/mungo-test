4d53ba052a0a956542ae426f8d3bea051762e95b
Euronite <leo@kumagai.co.uk>
2021-08-31 22:21:55 +0000

    Expanded user info to embed

--- commands/basic/user-info.js
+++ commands/basic/user-info.js
@@ -1,10 +1,33 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const avatar = require('./avatar');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Display info about yourself.'),
+		.setDescription('Display info about yourself.')
+		.addUserOption(option => option.setName('target').setDescription('The user\'s information to show')),
 	async execute(interaction) {
-		return interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
+		let user = interaction.options.getUser('target');
+		const avatarEmbed = new MessageEmbed()
+		.setColor('0099ff')
+		if (user){
+			pass
+		} else {
+			user = interaction.user
+		}
+		avatarEmbed.setTitle(`Here is ${user.username}'s avatar:`)
+		avatarEmbed.addFields(
+			{name:"Username", value:interaction.user.username}, 
+			{name:"User ID", value:`\`${interaction.user.id}\``},
+			{name:"User Tag", value:`\`${interaction.user.tag}\``},
+			{name:"Is Bot?", value:interaction.user.bot.toString()},
+			{name:"Official Discord System User", value:interaction.user.system.toString()},
+			{name:"Account Creation", value:interaction.user.createdAt.toString()}
+			)
+		avatarEmbed.addField("Roles:", interaction.member.roles.cache.map(roles => `${roles}`).join(', '), true)
+		//console.log(interaction.member.roles.cache)
+		avatarEmbed.setThumbnail(user.displayAvatarURL({dynamic:true}))
+		await interaction.reply({embeds: [avatarEmbed]})
 	},
 };
