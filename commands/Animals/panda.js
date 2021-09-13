1b44133217e2fa08051f14c2730bcc6eee4560d0
Euronite <leo@kumagai.co.uk>
2021-09-13 18:27:29 +0000

    panda command added

---
+++ commands/Animals/panda.js
@@ -0,0 +1,25 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
+const fetch = (...args) => import('node-fetch').then((
+    {default: fetch}) => fetch(...args),
+);
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('panda')
+      .setDescription('Get images of pandas'),
+  async execute(interaction) {
+    try {
+      result = await fetch('https://some-random-api.ml/img/panda').then((response) => response.json());
+    } catch (error) {
+      console.log(error);
+    }
+    if (result === undefined) {
+      return interaction.reply('Error occurred, try again later or report to developer.');
+    }
+    const embed = new MessageEmbed()
+        .setColor('0099ff')
+        .setTitle('Here\'s a panda image:')
+        .setImage(result.link);
+    await interaction.reply({embeds: [embed]});
+  },
+};
