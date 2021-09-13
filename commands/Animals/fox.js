13b6f680bc2bce57a7446765ab7a1e7a6dc57d28
Euronite <leo@kumagai.co.uk>
2021-09-13 18:23:40 +0000

    Fox command added

---
+++ commands/Animals/fox.js
@@ -0,0 +1,26 @@
+
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
+const fetch = (...args) => import('node-fetch').then((
+    {default: fetch}) => fetch(...args),
+);
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('fox')
+      .setDescription('Get images of foxes'),
+  async execute(interaction) {
+    try {
+      result = await fetch('https://some-random-api.ml/img/fox').then((response) => response.json());
+    } catch (error) {
+      console.log(error);
+    }
+    if (result === undefined) {
+      return interaction.reply('Error occurred, try again later or report to developer.');
+    }
+    const embed = new MessageEmbed()
+        .setColor('0099ff')
+        .setTitle('Here\'s a fox image:')
+        .setImage(result.link);
+    await interaction.reply({embeds: [embed]});
+  },
+};
