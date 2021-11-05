ef85f1ddcd59e423d3ee4a7d6c605d5561ba4ba5
Euronite <leo@kumagai.co.uk>
2021-11-05 22:18:22 +0000

    Adds Red Panda command

---
+++ commands/Animals/redpanda.js
@@ -0,0 +1,25 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
+const fetch = (...args) => import('node-fetch').then((
+    {default: fetch}) => fetch(...args),
+);
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('redpanda')
+      .setDescription('Get images of Red Pandas'),
+  async execute(interaction) {
+    try {
+      result = await fetch('https://some-random-api.ml/img/red_panda').then((response) => response.json());
+    } catch (error) {
+      console.log(error);
+    }
+    if (result === undefined) {
+      return interaction.reply('Error occurred, try again later or report to developer.');
+    }
+    const embed = new MessageEmbed()
+        .setColor('0099ff')
+        .setTitle('Here\'s a Red Panda image:')
+        .setImage(result.link);
+    await interaction.reply({embeds: [embed]});
+  },
+};
